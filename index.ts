6140f59e47acde98f03c71d391552d74a1d5d135
David Buckley <davidjaybuckley@gmail.com>
2020-01-31 09:53:17 -0700

    uuid and url

--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@ import * as AWS from 'aws-sdk';
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as util from 'util';
-import * as uuidv4 from 'uuid/v4';
+import { uuid } from 'uuidv4';
 
 const readFile = util.promisify(fs.readFile);
 
-const BUCKET_NAME = 'pot-of-gold-at-the-end-of-the-rainbow';
+const BUCKET_NAME = 'realreact-test';
 
 dotenv.config();
 const s3 = new AWS.S3({
@@ -16,7 +16,7 @@ const s3 = new AWS.S3({
 });
 
 const uploadToS3 = async (data: Buffer): Promise<string> => {
-    const name = uuidv4() + '.jpeg';
+    const name = uuid() + '.jpeg';
     await s3.putObject({
         Key: name,
         Bucket: BUCKET_NAME,
@@ -24,13 +24,14 @@ const uploadToS3 = async (data: Buffer): Promise<string> => {
         Body: data,
         ACL: 'public-read',
     }).promise();
-    return name;
+    return `https://${BUCKET_NAME}.s3-${process.env.AWS_REGION}.amazonaws.com/${name}`;
 }
 
 const main = async () => {
     try {
         const data = await readFile('./image.jpeg');
         const url = await uploadToS3(data);
+        console.log(url)
     } catch (err) {
         console.log(err);
     }
